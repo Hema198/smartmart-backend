4090a500380e14f32e542c643752b0c14acb9f02
Hema <hema.erfolg@gmail.com>
2025-06-16 19:55:40 +0530

    changes made in application.py

--- application.py
+++ application.py
@@ -6,6 +6,6 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     try:
-        return "✅ SmartMart is LIVE! 💛 Powered by Noor"
+        return "✅ SmartMart is LIVE! 💛 Powered by Noor and Aarav"
     except Exception as e:
         return f"❌ Error: {str(e)}\n\n{traceback.format_exc()}"
